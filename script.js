34d0bcbb35ce1b11e4cf455478aab16a4a4237c5
Francesca Salvini <franci98sp@gmail.com>
2021-04-20 15:34:24 +0200

    creata infrastruttura

---
+++ script.js
@@ -0,0 +1,14 @@
+// CONSEGNA
+
+// ESERCIZIO: Creare uno slider di immagini
+// BONUS: Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+
+var app = new Vue(
+    {
+        el: '#root',
+        data: {},
+        methods: {},
+        created() {}
+    }
+);
+
